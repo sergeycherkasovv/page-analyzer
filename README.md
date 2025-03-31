ef35436182ff87e1a4e6094861d4c0801c7bde5e
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-03-31 21:45:22 +0700

    add SonarQube

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
 ### My tests and linter status:
 [![my-check](https://github.com/sergeycherkasovv/java-project-72/actions/workflows/blank.yml/badge.svg)](https://github.com/sergeycherkasovv/java-project-72/actions/workflows/blank.yml)
+
+### SonarQube
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=sergeycherkasovv_java-project-72&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=sergeycherkasovv_java-project-72)
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     checkstyle
+    id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "hexlet.code"
@@ -13,6 +14,14 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+sonar {
+ properties {
+ property("sonar.projectKey", "sergeycherkasovv_java-project-72")
+ property("sonar.organization", "sergeycherkasovv")
+ property("sonar.host.url", "https://sonarcloud.io")
+ }
 }
 
 tasks.test {
