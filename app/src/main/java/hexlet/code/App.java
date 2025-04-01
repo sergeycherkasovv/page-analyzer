c93dcd56806967967e01dc397cfa77be16cb767e
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-01 23:06:14 +0700

    create method getPort in App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,11 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class App {
-    public static final int PORT = 7070;
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
 
     public static void main(String[] args) throws IOException {
         Javalin app = getApp();
-        app.start(PORT);
+        app.start(getPort());
     }
     public static Javalin getApp() throws IOException {
         var app = Javalin.create(config -> {
