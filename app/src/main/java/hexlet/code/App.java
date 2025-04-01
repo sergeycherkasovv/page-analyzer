5bf59ba4f8296aff405ee62bd2c174bbc2b01c41
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-01 22:59:00 +0700

    changed App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,10 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class App {
+    public static final int PORT = 7070;
 
     public static void main(String[] args) throws IOException {
         Javalin app = getApp();
-        app.start(7070);
+        app.start(PORT);
     }
     public static Javalin getApp() throws IOException {
         var app = Javalin.create(config -> {
