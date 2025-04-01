fb8f7db90ea878087a5a7463abdd72329d1e35a8
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-01 22:52:22 +0700

    enable application, shadow, slf4j, javalin, lombok

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,20 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("java")
     checkstyle
+    application
     id("org.sonarqube") version "6.0.1.5171"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
 
 repositories {
     mavenCentral()
@@ -14,6 +23,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
+    implementation("io.javalin:javalin:6.1.3")
+    compileOnly("org.projectlombok:lombok:1.18.36")
 }
 
 sonar {
@@ -26,4 +38,11 @@ sonar {
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
