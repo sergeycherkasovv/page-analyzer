28630d6e62c36dfc59d00cfdd7dbba001b0f14de
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-02 19:46:50 +0700

    add model/Url.java

---
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,19 @@
+package hexlet.code.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class Url {
+    private Long id;
+    private String name;
+    private LocalDateTime createdAt;
+
+    public Url(String name) {
+        this.name = name;
+    }
+}
