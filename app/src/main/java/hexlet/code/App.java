151e7c0c27486e41a7deaa6d42ebaabf79874bd1
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-02 20:46:38 +0700

    added Hikari Config, getUrl method, readResourceFile method to App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,17 @@
 package hexlet.code;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -13,11 +22,38 @@ public class App {
         return Integer.valueOf(port);
     }
 
-    public static void main(String[] args) throws IOException {
+    private static String getUrl() {
+        String url = System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:Url");
+
+        return url;
+    }
+
+    private static String readResourceFile(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+
+    public static void main(String[] args) throws IOException, SQLException {
         Javalin app = getApp();
         app.start(getPort());
     }
-    public static Javalin getApp() throws IOException {
+    public static Javalin getApp() throws IOException, SQLException {
+
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(getUrl());
+
+        var dataSource = new HikariDataSource(hikariConfig);
+        var sql = readResourceFile("schema.sql");
+
+        log.info(sql);
+        try (var connection = dataSource.getConnection();
+            var statement = connection.createStatement())    {
+            statement.execute(sql);
+        }
+        BaseRepository.dataSource = dataSource;
+
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
