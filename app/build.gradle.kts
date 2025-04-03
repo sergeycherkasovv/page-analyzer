3fadaff5f2ed0887fa58840233e5d459ecf5e36e
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-03 23:44:33 +0700

    changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,12 +23,12 @@ repositories {
 
 dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.16")
-    implementation("io.javalin:javalin:6.5.0")
+    implementation("io.javalin:javalin:6.3.0")
     implementation("com.zaxxer:HikariCP:6.3.0")
-    testImplementation("com.h2database:h2:2.2.224")
+    testImplementation("com.h2database:h2:2.3.232")
     implementation("org.postgresql:postgresql:42.7.4")
 
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.11.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
