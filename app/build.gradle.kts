79b54e04ef484b6aad8c1f69844efa78af0f5389
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-06 21:39:04 +0700

    connected jte, javalin-render, javalin-bundle in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,10 @@ repositories {
 dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.16")
     implementation("io.javalin:javalin:6.5.0")
+        implementation("io.javalin:javalin-bundle:6.5.0")
+    implementation("io.javalin:javalin-rendering:6.5.0")
+    implementation("gg.jte:jte:3.2.0")
+
     implementation("com.zaxxer:HikariCP:6.3.0")
     implementation("com.h2database:h2:2.3.232")
     implementation("org.postgresql:postgresql:42.7.4")
