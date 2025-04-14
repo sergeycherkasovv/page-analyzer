82e0e60897a0123337adfba65aedc2db314e70aa
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-14 22:47:04 +0700

    changed method index in RootController.java

--- app/src/main/java/hexlet/code/controller/RootController.java
+++ app/src/main/java/hexlet/code/controller/RootController.java
@@ -1,10 +1,20 @@
 package hexlet.code.controller;
 
 
+import hexlet.code.dto.urls.UrlsPage;
+import hexlet.code.repository.UrlsRepository;
 import io.javalin.http.Context;
 
+import java.sql.SQLException;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+
 public class RootController {
-    public static void index (Context ctx) {
-        ctx.render("index.jte");
+    public static void index (Context ctx) throws SQLException {
+        var urls = UrlsRepository.getEntities();
+        var page = new UrlsPage(urls);
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlashType(ctx.consumeSessionAttribute("flash-type"));
+        ctx.render("index.jte", model("page", page));
     }
 }
