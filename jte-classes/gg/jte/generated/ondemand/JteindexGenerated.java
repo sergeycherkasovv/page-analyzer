131e8aa1b8f45a3a604b0ea340bf4f35d3d92cc9
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-14 22:46:13 +0700

    changed index.jte

--- jte-classes/gg/jte/generated/ondemand/JteindexGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/JteindexGenerated.java
@@ -4,7 +4,7 @@ import hexlet.code.util.NamedRoutes;
 @SuppressWarnings("unchecked")
 public final class JteindexGenerated {
 	public static final String JTE_NAME = "index.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,1,2,2,2,2,4,4,6,6,13,13,13,13,13,13,13,13,13,33,33,33,34,34,34,2,2,2,2};
+	public static final int[] JTE_LINE_INFO = {0,0,1,2,2,2,2,4,4,7,7,14,14,14,14,14,14,14,14,14,34,34,34,35,35,35,2,2,2,2};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, UrlsPage page) {
 		jteOutput.writeContent("\n");
 		gg.jte.generated.ondemand.layout.JtepageGenerated.render(jteOutput, jteHtmlInterceptor, new gg.jte.html.HtmlContent() {
@@ -20,7 +20,7 @@ public final class JteindexGenerated {
 				}
 				jteOutput.writeContent(" method=\"post\" class=\"rss-form text-body\">\n                        <div class=\"row\">\n                            <div class=\"col\">\n                                <div class=\"form-floating\">\n                                    <input id=\"url-input\" autofocus=\"\" type=\"text\" required=\"\" name=\"url\"\n                                           aria-label=\"url\" class=\"form-control w-100\" placeholder=\"ссылка\"\n                                           autocomplete=\"off\">\n                                    <label for=\"url-input\">Ссылка</label>\n                                </div>\n                            </div>\n                            <div class=\"col-auto\">\n                                <button type=\"submit\" class=\"h-100 btn btn-lg btn-primary px-sm-5\">Проверить</button>\n                            </div>\n                        </div>\n                    </form>\n                    <p class=\"mt-2 mb-0 text-muted\">Пример: https://www.example.com</p>\n                </div>\n            </div>\n        </div>\n    </section>\n");
 			}
-		}, null);
+		}, page);
 		jteOutput.writeContent("\n");
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
--- jte-classes/gg/jte/generated/ondemand/layout/JtepageGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/layout/JtepageGenerated.java
@@ -5,9 +5,9 @@ import hexlet.code.util.NamedRoutes;
 @SuppressWarnings("unchecked")
 public final class JtepageGenerated {
 	public static final String JTE_NAME = "layout/page.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,1,2,3,3,3,3,23,23,23,23,23,23,23,23,23,23,26,26,26,26,26,26,26,26,26,27,27,27,27,27,27,27,27,27,33,33,34,34,34,34,35,35,35,37,37,39,39,39,50,50,50,3,4,4,4,4};
+	public static final int[] JTE_LINE_INFO = {0,0,1,2,3,3,3,3,26,26,26,26,26,26,26,26,26,26,29,29,29,29,29,29,29,29,29,30,30,30,30,30,30,30,30,30,36,36,37,37,37,37,38,38,38,41,41,43,43,43,54,54,54,3,4,4,4,4};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, Content content, BasePage page) {
-		jteOutput.writeContent("\n<!doctype html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"UTF-8\">\n    <meta name=\"viewport\"\n          content=\"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\">\n    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\n    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\"\n          rel=\"stylesheet\"\n          integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\"\n          crossorigin=\"anonymous\">\n\n    <title>Page Analyzer</title>\n</head>\n<body class=\"d-flex flex-column min-vh-100\">\n        <nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">\n          <div class=\"container-fluid\">\n            <a class=\"navbar-brand\"");
+		jteOutput.writeContent("\n<!doctype html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"UTF-8\">\n    <meta name=\"viewport\"\n          content=\"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\">\n    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\n    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\"\n          rel=\"stylesheet\"\n          integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\"\n          crossorigin=\"anonymous\">\n    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js\"\n            integrity=\"sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4\"\n            crossorigin=\"anonymous\"></script>\n\n    <title>Page Analyzer</title>\n</head>\n<body class=\"d-flex flex-column min-vh-100\">\n        <nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">\n          <div class=\"container-fluid\">\n            <a class=\"navbar-brand\"");
 		var __jte_html_attribute_0 = NamedRoutes.rootPath();
 		if (gg.jte.runtime.TemplateUtils.isAttributeRendered(__jte_html_attribute_0)) {
 			jteOutput.writeContent(" href=\"");
@@ -36,14 +36,14 @@ public final class JtepageGenerated {
 		}
 		jteOutput.writeContent(">Сайты</a>\n              </div>\n            </div>\n          </div>\n        </nav>\n\n        ");
 		if (page != null && page.getFlash() != null) {
-			jteOutput.writeContent("\n            <div class=\"alert alert-");
+			jteOutput.writeContent("\n            <div class=\"rounded-0 m-0 alert alert-dismissible fade show alert-");
 			jteOutput.setContext("div", "class");
 			jteOutput.writeUserContent(page.getFlashType());
 			jteOutput.setContext("div", null);
-			jteOutput.writeContent("\" role=\"alert\">\n                ");
-			jteOutput.setContext("div", null);
+			jteOutput.writeContent("\" role=\"alert\">\n                <p class=\"m-0\">");
+			jteOutput.setContext("p", null);
 			jteOutput.writeUserContent(page.getFlash());
-			jteOutput.writeContent("\n            </div>\n        ");
+			jteOutput.writeContent("</p>\n                <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button>\n            </div>\n        ");
 		}
 		jteOutput.writeContent("\n\n        <main class=\"flex-grow-1\">");
 		jteOutput.setContext("main", null);
