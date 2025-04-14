9a710353757b420ca90d8ea2500f1fea5bd4d640
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-14 16:52:32 +0700

    changed href in page.jte

--- jte-classes/gg/jte/generated/ondemand/layout/JtepageGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/layout/JtepageGenerated.java
@@ -5,7 +5,7 @@ import hexlet.code.util.NamedRoutes;
 @SuppressWarnings("unchecked")
 public final class JtepageGenerated {
 	public static final String JTE_NAME = "layout/page.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,1,2,3,3,3,3,23,23,23,23,23,23,23,23,23,23,26,26,26,26,26,26,26,26,26,33,33,34,34,34,34,35,35,35,37,37,39,39,39,50,50,50,3,4,4,4,4};
+	public static final int[] JTE_LINE_INFO = {0,0,1,2,3,3,3,3,23,23,23,23,23,23,23,23,23,23,26,26,26,26,26,26,26,26,26,27,27,27,27,27,27,27,27,27,33,33,34,34,34,34,35,35,35,37,37,39,39,39,50,50,50,3,4,4,4,4};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, Content content, BasePage page) {
 		jteOutput.writeContent("\n<!doctype html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"UTF-8\">\n    <meta name=\"viewport\"\n          content=\"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\">\n    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\n    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\"\n          rel=\"stylesheet\"\n          integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\"\n          crossorigin=\"anonymous\">\n\n    <title>Page Analyzer</title>\n</head>\n<body class=\"d-flex flex-column min-vh-100\">\n        <nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">\n          <div class=\"container-fluid\">\n            <a class=\"navbar-brand\"");
 		var __jte_html_attribute_0 = NamedRoutes.rootPath();
@@ -25,7 +25,16 @@ public final class JtepageGenerated {
 			jteOutput.setContext("a", null);
 			jteOutput.writeContent("\"");
 		}
-		jteOutput.writeContent(">Главная</a>\n                <a class=\"nav-link\" href=\"#\">Сайты</a>\n              </div>\n            </div>\n          </div>\n        </nav>\n\n        ");
+		jteOutput.writeContent(">Главная</a>\n                <a class=\"nav-link\"");
+		var __jte_html_attribute_2 = NamedRoutes.urlsPath();
+		if (gg.jte.runtime.TemplateUtils.isAttributeRendered(__jte_html_attribute_2)) {
+			jteOutput.writeContent(" href=\"");
+			jteOutput.setContext("a", "href");
+			jteOutput.writeUserContent(__jte_html_attribute_2);
+			jteOutput.setContext("a", null);
+			jteOutput.writeContent("\"");
+		}
+		jteOutput.writeContent(">Сайты</a>\n              </div>\n            </div>\n          </div>\n        </nav>\n\n        ");
 		if (page != null && page.getFlash() != null) {
 			jteOutput.writeContent("\n            <div class=\"alert alert-");
 			jteOutput.setContext("div", "class");
