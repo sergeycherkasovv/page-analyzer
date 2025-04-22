6ddb5978de054983586e3657879d5102ff105d9d
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-23 00:20:23 +0700

    created ReadFiles.java

---
+++ app/src/main/java/hexlet/code/util/ReadFiles.java
@@ -0,0 +1,19 @@
+package hexlet.code.util;
+
+import hexlet.code.App;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+public class ReadFiles {
+    public static String readResourceFile(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+}
