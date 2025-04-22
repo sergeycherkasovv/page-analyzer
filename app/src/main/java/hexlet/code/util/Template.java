a7e10b76a4f9767b3044365366989ba8e4fa3a32
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-23 00:22:40 +0700

    relocate Template.java and changed modificator method

--- app/src/main/java/hexlet/code/util/Template.java
+++ app/src/main/java/hexlet/code/util/Template.java
@@ -1,11 +1,12 @@
-package hexlet.code;
+package hexlet.code.util;
 
 import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
 import gg.jte.resolve.ResourceCodeResolver;
+import hexlet.code.App;
 
 public class Template {
-    protected static TemplateEngine createTemplateEngine() {
+    public static TemplateEngine createTemplateEngine() {
         ClassLoader classLoader = App.class.getClassLoader();
         ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
         TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
