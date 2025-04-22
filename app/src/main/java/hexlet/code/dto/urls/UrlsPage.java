3782d1f756d6e726905b6c29c387414626afe47d
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-23 00:27:32 +0700

    add variable 'lastCheck' in UrlsPage.java

--- app/src/main/java/hexlet/code/dto/urls/UrlsPage.java
+++ app/src/main/java/hexlet/code/dto/urls/UrlsPage.java
@@ -2,13 +2,16 @@ package hexlet.code.dto.urls;
 
 import hexlet.code.dto.BasePage;
 import hexlet.code.model.Url;
-import lombok.AllArgsConstructor;
+import hexlet.code.model.UrlCheck;
 import lombok.Getter;
+import lombok.AllArgsConstructor;
 
 import java.util.List;
+import java.util.Map;
 
 @AllArgsConstructor
 @Getter
 public class UrlsPage extends BasePage {
-    private List<Url> urls;
+    private final List<Url> urls;
+    private final Map<Long, UrlCheck> lastCheck;
 }
