3b6794243a9e8968eccd57d02c01220717824d22
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-23 00:20:37 +0700

    changed RootController.java

--- app/src/main/java/hexlet/code/controller/RootController.java
+++ app/src/main/java/hexlet/code/controller/RootController.java
@@ -2,6 +2,7 @@ package hexlet.code.controller;
 
 
 import hexlet.code.dto.urls.UrlsPage;
+import hexlet.code.repository.UrlChecksRepository;
 import hexlet.code.repository.UrlsRepository;
 import io.javalin.http.Context;
 
@@ -12,9 +13,12 @@ import static io.javalin.rendering.template.TemplateUtil.model;
 public class RootController {
     public static void index(Context ctx) throws SQLException {
         var urls = UrlsRepository.getEntities();
-        var page = new UrlsPage(urls);
+        var urlLastCheck = UrlChecksRepository.findLatestChecks();
+        var page = new UrlsPage(urls, urlLastCheck);
+
         page.setFlash(ctx.consumeSessionAttribute("flash"));
         page.setFlashType(ctx.consumeSessionAttribute("flash-type"));
+
         ctx.render("index.jte", model("page", page));
     }
 }
