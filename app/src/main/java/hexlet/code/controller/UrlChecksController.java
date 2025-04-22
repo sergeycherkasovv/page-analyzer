c1d7a260b0a7618d9071a93a4da590ccff275d4f
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-23 00:23:55 +0700

    created UrlChecksController.java

---
+++ app/src/main/java/hexlet/code/controller/UrlChecksController.java
@@ -0,0 +1,37 @@
+package hexlet.code.controller;
+
+import hexlet.code.model.UrlCheck;
+import hexlet.code.repository.UrlChecksRepository;
+import hexlet.code.repository.UrlsRepository;
+import hexlet.code.util.NamedRoutes;
+import hexlet.code.util.UrlUtils;
+import io.javalin.http.Context;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class UrlChecksController {
+    public static void check(Context ctx) throws SQLException {
+        var urlId = Long.valueOf(ctx.pathParam("id"));
+        var url = UrlsRepository.find(urlId).orElseThrow();
+
+        HttpResponse<String> response = Unirest.get(url.getName()).asString();
+
+        int status = response.getStatus();
+
+        Document doc = Jsoup.parse(response.getBody());
+        String h1 = doc.select("h1").text();
+        String title = doc.title();
+        String description = doc.select("meta[name=description]").attr("content");
+        var createdAt = new Timestamp(System.currentTimeMillis());
+
+        var check = new UrlCheck(urlId, status, title, h1, description, createdAt);
+        UrlChecksRepository.save(check);
+        UrlUtils.alertFlash(ctx, "Страница успешно проверена", "success");
+        ctx.redirect(NamedRoutes.urlPath(urlId));
+    }
+}
