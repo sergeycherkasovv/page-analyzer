54aef12aedf2033eda2700410f267ea27b252a3d
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-23 00:20:04 +0700

    add methods urlPathChecks()

--- app/src/main/java/hexlet/code/util/NamedRoutes.java
+++ app/src/main/java/hexlet/code/util/NamedRoutes.java
@@ -16,4 +16,12 @@ public class NamedRoutes {
     public static String urlPath(String id) {
         return "/urls/" + id;
     }
+
+    public static String urlPathChecks(Long id) {
+        return urlPathChecks(String.valueOf(id));
+    }
+
+    public static String urlPathChecks(String id) {
+        return "/urls/" + id + "/checks";
+    }
 }
