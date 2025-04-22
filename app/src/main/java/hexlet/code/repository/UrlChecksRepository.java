9dbc01f829fac58d8089d0c0e35721dd221b0d0a
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-23 00:24:16 +0700

    created UrlChecksRepository.java.java

---
+++ app/src/main/java/hexlet/code/repository/UrlChecksRepository.java
@@ -0,0 +1,90 @@
+package hexlet.code.repository;
+
+import hexlet.code.model.UrlCheck;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static hexlet.code.repository.BaseRepository.dataSource;
+
+public class UrlChecksRepository {
+
+    public static void save(UrlCheck check) throws SQLException {
+        String sql = "INSERT INTO url_checks (url_id, status_code, h1, title, description, created_at)"
+                    + " VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (var conn = dataSource.getConnection();
+                var stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            stmt.setLong(1, check.getUrlId());
+            stmt.setLong(2, check.getStatusCode());
+            stmt.setString(3, check.getH1());
+            stmt.setString(4, check.getTitle());
+            stmt.setString(5, check.getDescription());
+            stmt.setTimestamp(6, check.getCreatedAt());
+            stmt.executeUpdate();
+
+            var generatedKeys = stmt.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                check.setId(generatedKeys.getLong(1));
+            } else {
+                throw new SQLException("BD have not returned an id after saving an entity");
+            }
+        }
+    }
+
+    public static List<UrlCheck> getEntities() throws SQLException {
+        String sql = "SELECT * FROM url_checks";
+
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+
+            var resultSet = stmt.executeQuery();
+            var result = new ArrayList<UrlCheck>();
+
+            while (resultSet.next()) {
+                var id = resultSet.getLong("id");
+                var urlId = resultSet.getLong("url_id");
+                var statusCode = resultSet.getInt("status_code");
+                var h1 = resultSet.getString("h1");
+                var title = resultSet.getString("title");
+                var description = resultSet.getString("description");
+                var createdAt = resultSet.getTimestamp("created_at");
+                var urlCheck = new UrlCheck(urlId, statusCode, h1, title, description, createdAt);
+                urlCheck.setId(id);
+                result.add(urlCheck);
+            }
+            return result;
+        }
+    }
+
+    public static Map<Long, UrlCheck> findLatestChecks() throws SQLException {
+        var map = new HashMap<Long, UrlCheck>();
+        var sql = "SELECT * FROM url_checks ORDER BY id";
+
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+
+            var resultSet = stmt.executeQuery();
+
+            while (resultSet.next()) {
+                var checkId = resultSet.getLong("id");
+                var urlId = resultSet.getLong("url_id");
+                var statusCode = resultSet.getInt("status_code");
+                var h1 = resultSet.getString("h1");
+                var title = resultSet.getString("title");
+                var description = resultSet.getString("description");
+                var createdAt = resultSet.getTimestamp("created_at");
+
+                var urlCheck = new UrlCheck(urlId, statusCode, title, h1, description, createdAt);
+                urlCheck.setId(checkId);
+                map.put(urlId, urlCheck);
+            }
+            return map;
+        }
+    }
+}
