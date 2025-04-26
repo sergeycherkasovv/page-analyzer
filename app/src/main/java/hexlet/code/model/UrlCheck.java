3af73540108c87b4fc65272b682931b714f659bd
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-04-26 15:58:09 +0700

    change UrlCheck.java

--- app/src/main/java/hexlet/code/model/UrlCheck.java
+++ app/src/main/java/hexlet/code/model/UrlCheck.java
@@ -19,13 +19,12 @@ public class UrlCheck implements DataFormated {
     private String description;
     private Timestamp createdAt;
 
-    public UrlCheck(Long urlId, int statusCode, String title, String h1, String description, Timestamp createdAt) {
+    public UrlCheck(Long urlId, int statusCode, String title, String h1, String description) {
         this.urlId = urlId;
         this.statusCode = statusCode;
         this.title = title;
         this.h1 = h1;
         this.description = description;
-        this.createdAt = createdAt;
     }
 
     @Override
