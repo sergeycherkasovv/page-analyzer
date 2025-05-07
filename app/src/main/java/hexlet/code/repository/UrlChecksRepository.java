7742079578c98d25c8bb168f8f207854cdd8f1b6
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-05-07 12:20:12 +0700

    changed method 'findLatestChecks()' in UrlChecksRepository.java

--- app/src/main/java/hexlet/code/repository/UrlChecksRepository.java
+++ app/src/main/java/hexlet/code/repository/UrlChecksRepository.java
@@ -68,8 +68,8 @@ public class UrlChecksRepository {
 
     public static Map<Long, UrlCheck> findLatestChecks() throws SQLException {
         var map = new HashMap<Long, UrlCheck>();
-        var sql = "SELECT DISTINCT ON (url_id) * " +
-                "FROM url_checks ORDER BY url_id, created_at DESC";
+        var sql = "SELECT DISTINCT ON (url_id) * "
+                + "FROM url_checks ORDER BY url_id, created_at DESC";
 
         try (var conn = dataSource.getConnection();
              var stmt = conn.prepareStatement(sql)) {
